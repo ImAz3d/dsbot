ca40b67079a2985ae91ec8e91e8849ba0768c2ce
ImAz3d <43502190+ImAz3d@users.noreply.github.com>
2018-09-22 21:18:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,19 +15,19 @@ if(message.content.startsWith(prefix+"dimealgo"))
   
 if (message.content.startsWith(prefix+"sobreds")){
     const embed = new Discord.RichEmbed()
-    .setTitle("Official DS TEAM Discord Server")
+     .setTitle("Este es su título, puede contener 256 caracteres")
     .setAuthor(message.author.username, message.author.avatarURL)
     .setColor(0x00AE86)
-    .setDescription("Discord Oficial de DS TEAM.")
-    .setFooter("Enviado por Ronin!", client.user.avatarURL)
+    .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
+    .setFooter("Pie de página, puede contener 2048 caracteres", client.user.avatarURL)
     .setImage(message.author.avatarURL)
-    .setURLThumbnail(message.author.avatarURL)
+    .setThumbnail(message.author.avatarURL)
     .setTimestamp()
-    .setURL("https://discord.gg/9k8eDWw")
-    .addField("Sobre el soporte y el servidor oficial",
-      "Entra a DS TEAM para soporte de Ronin! o ser parte de la comunidad, en caso de alguna duda o error sobre Ronin! consulta a los Guardianes de DS o Al Creador.", false)
-    .addBlankField(true);
-
+    .setURL("https://github.com/CraterMaik")
+    .addField("Este es un título de campo", "Este es un valor de campo puede contener 2048 caracteres.")
+    .addField("Campo en línea", "Debajo del campo en línea", true)
+    .addBlankField(true)
+    .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
     message.channel.send(embed)
 }
 });
