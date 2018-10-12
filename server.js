1124fec8eeb267d3bb0696812b6bfa8d85f0caf9
ImAz3d <43502190+ImAz3d@users.noreply.github.com>
2018-10-12 20:17:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ if (message.content.startsWith(prefix+"sobreds")){
 }
 });
  client.login(datos.token);
- client.login(process.env.TOKEN);
+ client.login(process.env.BOT_TOKEN);
 
  client.on("error", (e) => console.error(e));
  client.on("warn", (e) => console.warn(e));
