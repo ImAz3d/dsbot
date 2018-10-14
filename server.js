b24a3c1a3c0a6396dd1e9124c3e6ac4198865653
ImAz3d <43502190+ImAz3d@users.noreply.github.com>
2018-10-14 04:41:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if(message.content.startsWith(prefix+"help"))
 if(message.content.startsWith(prefix+"invitebot"))
    embed=discord.Embed(title="Click aqui para agregar al bot a tu servidor", url="https://discordapp.com/api/oauth2/authorize?client_id=492970132654784523&permissions=8&scope=bot", color=0x80ff80)
    .set_thumbnail(url="https://cdn.discordapp.com/attachments/448872120710070287/490783433857957895/GIF.gif")
-   .add_field(name=Te gusto el bot?, value=Apoya al creador en el server de soporte!, inline=True)
+   .add_field(name= Te gusto el bot?, value= Apoya al creador en el server de soporte!, inline=True)
    .set_footer(text="Ronin (DM Bot) by ! A̖̱̖̳̤͜z̭̣̯͕̻3̣̪͉͕̫͞d̲̪͝#6086")
    await self.bot.say(embed=embed)
 
