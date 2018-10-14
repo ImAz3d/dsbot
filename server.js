513e648f8020a880a58a590a0d95eecc82636b8f
ImAz3d <43502190+ImAz3d@users.noreply.github.com>
2018-10-14 06:31:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -93,12 +93,7 @@ if(command === "addrole"){
       }
     }
 });
-}
-     
-   
-  }
-    
-});
+} 
  client.on("error", (e) => console.error(e));
  client.on("warn", (e) => console.warn(e));
  client.on("debug", (e) => console.info(e));
