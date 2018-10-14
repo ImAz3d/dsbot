c9861e4a5060f075f95db8d6e3e5cd1ed084ad61
ImAz3d <43502190+ImAz3d@users.noreply.github.com>
2018-10-14 04:50:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -30,26 +30,48 @@ if(message.content.startsWith(prefix+"help"))
 if(message.content.startsWith(prefix+"invitebot"))
    embed=discord.Embed(title="Click aqui para agregar al bot a tu servidor", url="https://discordapp.com/api/oauth2/authorize?client_id=492970132654784523&permissions=8&scope=bot", color=0x80ff80)
    .set_thumbnail(url="https://cdn.discordapp.com/attachments/448872120710070287/490783433857957895/GIF.gif")
-   .add_field(name= Te gusto el bot?, value= Apoya al creador en el server de soporte!, inline=True)
    .set_footer(text="Ronin (DM Bot) by ! A̖̱̖̳̤͜z̭̣̯͕̻3̣̪͉͕̫͞d̲̪͝#6086")
    await self.bot.say(embed=embed)
 
-if(command === "addrole") {
-    if(!message.member.roles.some(r=>["♛Creador de DM♛"].includes(r.name)))
-    if(!message.member.roles.some(r=>["✪Guardian de DM✪"].includes(r.name)))
-    return message.channel.send("No tienes los permisos necesarios")
-    if(!args[0]) return message.channel.send("Necesitas colocar al @usuario/ID y el @rol/nombre")
-    var user = message.mentions.members.first() || message.guild.members.get(args[0])
-    if(!user) return message.channel.send(`el usuario ${args[0]} no existe`)
-    if(!args[1]) return message.channel.send("Necesitas colocar el @rol/nombre")
-    var role = message.mentions.roles.first() || message.guild.roles.find("name",args.slice(1).join(" "))
-    if(!role) return message.channel.send(`el rol ${args.slice(1).join(" ")} no existe.`)
-    user.addRole(role.id).then(m => {
-        message.channel.send("El usuario "+user+" acaba de recibir el rol "+role.name)
-          }).catch(error => {
-            console.log(error)
-        });
-    }
+if(command === "removerole"){
+
+    let miembro = message.mentions.members.first();
+    let nombrerol = args.slice(1).join(' ');
+
+    let role = message.guild.roles.find("name", nombrerol);
+    let perms = message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS");
+
+    if(!perms) return message.channel.send("`Error` `|` No tienes Permisos para usar este comando.");
+     
+    if(message.mentions.users.size < 1) return message.reply('Debe mencionar a un miembro.').catch(console.error);
+    if(!nombrerol) return message.channel.send('Escriba el nombre del rol a remover, `-removerol @miembro [rol]`');
+    if(!role) return message.channel.send('Rol no encontrado en el servidor.');
+    
+    miembro.removeRole(role).catch(console.error);
+    message.channel.send(`El rol **${role.name}** del miembro **${miembro.user.username}** fue removido  correctamente.`);
+
+  }
+
+
+if(command === "addrole"){
+
+    let miembro = message.mentions.members.first();
+    let nombrerol = args.slice(1).join(' ');
+
+    let role = message.guild.roles.find("name", nombrerol);
+    let perms = message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS");
+
+    if(!perms) return message.channel.send("`Error` `|` No tienes Permisos para usar este comando.");
+     
+    if(message.mentions.users.size < 1) return message.reply('Debe mencionar a un miembro.').catch(console.error);
+    if(!nombrerol) return message.channel.send('Escriba el nombre del rol a agregar, `-addrol @username [rol]`');
+    if(!role) return message.channel.send('Rol no encontrado en el servidor.');
+    
+    miembro.addRole(role).catch(console.error);
+    message.channel.send(`El rol **${role.name}** fue agregado correctamente a **${miembro.user.username}**.`);
+   
+  }
+    
 });
  client.on("error", (e) => console.error(e));
  client.on("warn", (e) => console.warn(e));
